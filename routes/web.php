587ca9b12a443f6f22889580b3ec3d3699676aaa
ilyakrup <ilyakrup1@yandex.ru>
2022-05-11 22:44:33 +0400

    фронт Админки

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'main'], function() {
+Route::group(['namespace' => 'main'], function () {
     Route::get('/', 'IndexController');
+});
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'main'], function () {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Auth::routes();

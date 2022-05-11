06cc52fdf065a5b4d642c99872b679e40aab45e2
ilyakrup <ilyakrup1@yandex.ru>
2022-05-11 21:42:33 +0400

    Подгрузил фронт блога

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'main'], function() {
+    Route::get('/', 'IndexController');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
